f783672d8032660bbd53bd4ae4c9fba36ac35c71
rbals37 <pmpm37gyu@gmail.com>
2024-09-09 00:50:20 +0900

    signup,login

--- PYGRAM/login/views.py
+++ PYGRAM/login/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
-#main
-def login(quest):
-    return render(quest,'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('main')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})

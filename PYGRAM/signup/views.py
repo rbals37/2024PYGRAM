a47afe6b1a5821ca65df7749a968b1dcb0c11eca
rbals37 <pmpm37gyu@gmail.com>
2024-09-10 00:53:11 +0900

    real complete

--- PYGRAM/signup/views.py
+++ PYGRAM/signup/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomSignupForm
 from django.contrib.auth import login as auth_login
 
 # Create your views here.
@@ -8,13 +8,13 @@ from django.contrib.auth import login as auth_login
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomSignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('main')
     else:
-         form = UserCreationForm()
+         form = CustomSignupForm()
 
     return render(request, 'signup.html', {'form': form})
          

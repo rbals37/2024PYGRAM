6562ef9c0d37a82593d62feee7e711b93739cddf
rbals37 <pmpm37gyu@gmail.com>
2024-09-12 16:49:41 +0900

    login complete

--- PYGRAM/login/views.py
+++ PYGRAM/login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib import messages
@@ -7,18 +7,16 @@ from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        student_id = request.POST.get('student_id')
+        password = request.POST.get('password')
+
+        user = authenticate(request, student_id=student_id, password=password)
+        if user is not None:
             auth_login(request, user)
             return redirect('main')
         else:
-            messages.error(request, '잘못 입력하셨습니다.')            
-
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'login.html', {'form': form})
+            messages.error(request, '잘못된 학번 또는 비밀번호입니다.')
+    return render(request, 'login.html')
 
 def logout_view(request):
     auth_logout(request)

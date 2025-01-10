a658386a40e844da6df216720169aa727e806a62
swoiun <swoiun@icloud.com>
2025-01-10 10:57:42 +0900

    settings.py 수정

--- PYGRAM/PYGRAM/settings.py
+++ PYGRAM/PYGRAM/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'PYGRAM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
